bb7578b31c657a7a96d98f90ad0b7dffb1817c4f
Arnaldo Lima <104681314+AdsUser01@users.noreply.github.com>
2022-05-03 23:21:24 -0300

    Add files via upload

---
+++ Classes/Bd_config.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prototipo1_GT;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+ 
+
+namespace Prototipo1_GT.Classes
+{
+
+    public static class Bd_config
+    {
+        public static MySqlConnection Abrir() //objeto
+        { //Classe de conexao 
+            string cone = "server=localhost; database=gerenciadort; user = root; password= 988205211Arnaldo; port=3312"; //string de conexao 
+            MySqlConnection con = new MySqlConnection(cone);
+            con.Open(); //Abre a conexao
+            return con;
+
+        }
+
+    }
+        
+        
+        
+    
+}
+
