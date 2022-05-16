ca5d1ea44488e7c219f0a462923e04b53d1b8691
Arnaldo Lima <104681314+Armaldo0802@users.noreply.github.com>
2022-05-15 21:26:28 -0300

    Classes

---
+++ Classes/Bd_config.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Data.SqlClient;
+
+
+
+
+namespace Prototipo1_GT.Classes
+{
+
+    public static class Bd_config
+    {
+        public static SqlConnection Abrir() //objeto
+        { //Classe de conexao 
+            string cone = @"Data Source=SOLID\SQLEXPRESS;Initial Catalog=Tarefa;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            SqlConnection con = new SqlConnection(cone);
+            con.Open(); //Abre a conexao
+            return con;
+
+        }
+
+
+
+    }
+
+
+
+
+}
+
---
+++ Classes/Pessoa.cs
@@ -0,0 +1,20 @@
+﻿namespace Prototipo1_GT.Classes
+{
+     public  class Pessoa
+    {
+      
+       public static int id;
+       
+
+
+
+      public  int Id 
+        {
+            get { return id; }
+            set { id = value; }
+        
+        }
+       
+      
+    }
+}
